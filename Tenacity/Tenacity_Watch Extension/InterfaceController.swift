708d9b3a0d438c48e1e42ae12a811cd541a31c24
ucirichiep <richiep@uci.edu>
2018-07-26 16:11:13 -0700

    Tapping

---
+++ Tenacity/Tenacity_Watch Extension/InterfaceController.swift
@@ -0,0 +1,43 @@
+//
+//  InterfaceController.swift
+//  Tenacity_Watch Extension
+//
+//  Created by PLL on 7/26/18.
+//  Copyright © 2018 PLL. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+    var x = 0
+    @IBOutlet var myLabel: WKInterfaceLabel!
+    
+    @IBAction func screenTap(_ sender: Any) {
+        x+=1
+        print(x)
+        myLabel.setText(String(x))
+    }
+    
+    
+    
+
+}
