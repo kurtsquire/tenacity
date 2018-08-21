1d3c456167cf72d55c9584ac91cfd44b41b52ccf
Zac <zacloutier@gmail.com>
2018-08-21 14:26:08 -0700

    Cleaned up code, removed unncessesary functions, converted TimeStamps to date objects

--- Tenacity/Tenacity_Watch Extension/InterfaceController.swift
+++ Tenacity/Tenacity_Watch Extension/InterfaceController.swift
@@ -14,35 +14,31 @@ class InterfaceController: WKInterfaceController {
 
     var hapticCount: Int = 5
     var tapCount = 0
-//    let seshItems: Dictionary = ["Inputs":[], "TimeStapms":[], "Pof":false]
-//    var cycleDic:  = [:]
-//    var seshGroups: Dictionary< Int, Dictionary< String, Any > > = [:]
     var seshGroups = [Int:[String:Any]] ()
     var current_cycle: [String] = []
-    var current_ts: [TimeInterval] = []
+    var current_ts: [Date] = []
     var cycleCount: Int = 0
-    
-    override func awake(withContext context: Any?) {
-        super.awake(withContext: context)
-        
-        // Configure interface objects here.
-    }
-    
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-    
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
-    
-    var x = 0
     @IBOutlet var myLabel: WKInterfaceLabel!
     
-    func getTime() -> TimeInterval {
-        return Date().timeIntervalSince1970
+    
+    @IBOutlet var screenTapp: WKTapGestureRecognizer!
+    
+    @IBAction func screenTap(_ sender: WKTapGestureRecognizer) {
+        current_ts.append(Date())
+        tapCount+=1
+        myLabel.setText(String(tapCount))
+        current_cycle.append("T")
+        hapticCallerTap()
+    }
+    
+    @IBOutlet var swipe: WKSwipeGestureRecognizer!
+    
+    @IBAction func swipe(_ sender2: WKSwipeGestureRecognizer) {
+        current_ts.append(Date())
+        tapCount+=1
+        myLabel.setText(String(tapCount))
+        current_cycle.append("S")
+        hapticCallerSwipe()
     }
     
     func hapticCallerTap() {
@@ -66,28 +62,20 @@ class InterfaceController: WKInterfaceController {
         }
     }
     
-    @IBOutlet var screenTapp: WKTapGestureRecognizer!
-    
-    @IBAction func screenTap(_ sender: WKTapGestureRecognizer) {
-        current_ts.append(getTime())
-        tapCount+=1
-//        print(String(tapCount)+" "+getTime().description)
-        myLabel.setText(String(tapCount))
-        current_cycle.append("T")
-        hapticCallerTap()
-    }
-    
-    @IBOutlet var swipe: WKSwipeGestureRecognizer!
-    
-    @IBAction func swipe(_ sender2: WKSwipeGestureRecognizer) {
-        tapCount+=1
-//        print(String(tapCount)+" "+getTime().description)
-        myLabel.setText(String(tapCount))
-        current_cycle.append("S")
-        hapticCallerSwipe()
-    }
-    
-    
+    //    override func awake(withContext context: Any?) {
+    //        super.awake(withContext: context)
+    //        // Configure interface objects here.
+    //    }
+    //
+    //    override func willActivate() {
+    //        // This method is called when watch view controller is about to be visible to user
+    //        super.willActivate()
+    //    }
+    //
+    //    override func didDeactivate() {
+    //        // This method is called when watch view controller is no longer visible
+    //        super.didDeactivate()
+    //    }
     
 
 }
