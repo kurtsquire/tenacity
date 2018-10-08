31a5cb47229a25a93e3a08673c56b3f343d4222b
Zac <zacloutier@gmail.com>
2018-10-08 15:23:08 -0700

    The purge

---
+++ TenacityV2/TenacityV2 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TenacityV2 WatchKit Extension
+//
+//  Created by PLL on 10/8/18.
+//  Copyright © 2018 PLL. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ TenacityV2/TenacityV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TenacityV2
+//
+//  Created by PLL on 10/8/18.
+//  Copyright © 2018 PLL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
