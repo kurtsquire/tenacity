2632054ca5aa4f1653aa17569a716fc09eda0ee4
ucirichiep <richiep@uci.edu>
2019-05-14 15:18:49 -0700

    Fixed Interval for breathing out
    No longer based on how long breathe is held

--- TenacityV2/TenacityV2 WatchKit Extension/BreatheR.swift
+++ TenacityV2/TenacityV2 WatchKit Extension/BreatheR.swift
@@ -17,6 +17,7 @@ var time = 0.0
 var correctCyclesTotal = 0
 var cycleTotal = 0
 var inGame = true
+var resetInterval = 2.5
 
 
 class BreatheR: WKInterfaceController, WCSessionDelegate  {
@@ -190,7 +191,8 @@ class BreatheR: WKInterfaceController, WCSessionDelegate  {
             totalBreaths += 1
             print("total breaths: " + String(totalBreaths))
             
-            animate(withDuration: (breatheInTime - 0.5)){
+            //Can use "(breatheInTime - offset)" or global var "resetInterval" for "withDuration"
+            animate(withDuration: resetInterval){
                 self.image.setRelativeWidth(CGFloat(self.startRelativeWidth), withAdjustment: 0)
                 self.image.setRelativeHeight(CGFloat(self.startRelativeHeight), withAdjustment: 0)
             }
