d70b54de446cfe0a1171d52c2e059fe3357dda48
ucirichiep <richiep@uci.edu>
2019-08-30 19:15:09 -0700

    line charts put on stat pages
    Line charts still need to be populated with data from realm... but they're on the correct screens!

--- TenacityV3/TenacityV3/LotusStatsViewController.swift
+++ TenacityV3/TenacityV3/LotusStatsViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import Charts
 
 class LotusStatsViewController: PhoneViewController{
     
@@ -17,8 +18,16 @@ class LotusStatsViewController: PhoneViewController{
     @IBOutlet weak var roundsPlayedLabel: UILabel!
     @IBOutlet weak var swipesLabel: UILabel!
     @IBOutlet weak var lotusSwipeLabel: UILabel!
+    @IBOutlet weak var timePlayedLineChart: LineChartView!
+    @IBOutlet weak var totalRoundsLineChart: LineChartView!
     
     lazy var lotusGraphCenter = lotusSwipeLabel.center
+    
+    var gamesInfo = [
+        1 : ( gameName: "Breathe Flow", gameData: expFlow, gameColor: UIColor(cgColor: breatheFGraphProgColor), gameGoal: 20.00 ),
+        0 : ( gameName: "Breathe Focus", gameData: expFocus, gameColor: UIColor(cgColor: breatheIGraphProgColor), gameGoal: 22.50 ),
+        2 : ( gameName: "Lotus", gameData: expLotus, gameColor: UIColor(cgColor: lotusGraphProgColor), gameGoal: 30.00 )
+    ]
     
     // ------------------------- TIME ----------------------------------
     var calendar = Calendar.autoupdatingCurrent
@@ -49,6 +58,52 @@ class LotusStatsViewController: PhoneViewController{
         weekStartTime = calendar.date(from: weekComponents)!
         
         refreshRealmData()
+        
+        // ---------------------- LINES -----------------------------------
+        
+        self.timePlayedLineChart.gridBackgroundColor = UIColor.white
+        
+        self.timePlayedLineChart.noDataText = "No data provided"
+        
+        self.timePlayedLineChart.doubleTapToZoomEnabled = false
+        
+        self.timePlayedLineChart.xAxis.labelPosition = XAxis.LabelPosition.bottom
+        
+        self.timePlayedLineChart.highlightPerTapEnabled = false
+        self.timePlayedLineChart.highlightPerDragEnabled = false
+        
+        self.timePlayedLineChart.xAxis.valueFormatter = IndexAxisValueFormatter(values: weekDays)
+        
+        self.timePlayedLineChart.xAxis.granularity = 1
+        self.timePlayedLineChart.legend.textColor = UIColor.white
+        self.timePlayedLineChart.xAxis.labelTextColor = UIColor.white
+        self.timePlayedLineChart.rightAxis.labelTextColor = UIColor.white
+        self.timePlayedLineChart.leftAxis.labelTextColor = UIColor.white
+        let timePlayedChart : LineChart = LineChart(lineChartView: timePlayedLineChart, goalColor: UIColor.red, gamesInfo: gamesInfo)
+        
+        timePlayedChart.drawGoalGraph(gameNum: 2)
+        
+        self.totalRoundsLineChart.gridBackgroundColor = UIColor.white
+        
+        self.totalRoundsLineChart.noDataText = "No data provided"
+        
+        self.totalRoundsLineChart.doubleTapToZoomEnabled = false
+        
+        self.totalRoundsLineChart.xAxis.labelPosition = XAxis.LabelPosition.bottom
+        
+        self.totalRoundsLineChart.highlightPerTapEnabled = false
+        self.totalRoundsLineChart.highlightPerDragEnabled = false
+        
+        self.totalRoundsLineChart.xAxis.valueFormatter = IndexAxisValueFormatter(values: weekDays)
+        
+        self.totalRoundsLineChart.xAxis.granularity = 1
+        self.totalRoundsLineChart.legend.textColor = UIColor.white
+        self.totalRoundsLineChart.xAxis.labelTextColor = UIColor.white
+        self.totalRoundsLineChart.rightAxis.labelTextColor = UIColor.white
+        self.totalRoundsLineChart.leftAxis.labelTextColor = UIColor.white
+        let totalRoundsChart : LineChart = LineChart(lineChartView: totalRoundsLineChart, goalColor: UIColor.red, gamesInfo: gamesInfo)
+        
+        totalRoundsChart.drawGoalGraph(gameNum: 2)
     }
     
     override func viewDidAppear(_ animated: Bool) { //openign back up the tab (works from other tabs)
@@ -87,13 +142,13 @@ class LotusStatsViewController: PhoneViewController{
         
         DispatchQueue.main.async {
             self.minuteGoalLabel.text = String(Int(lotusTimeToday/60)) + "/" + String(Int(lotusGoalTime)) + "mins"
-            self.timePlayedLabel.text = "Week: " + String(lotusTimeWeek) + "\nToday: " + String(lotusTimeToday)
-            self.roundsPlayedLabel.text = "Week:\n" + String(lotusRoundsWeek) + "\nToday:\n" + String(lotusRoundsToday)
-            var a = "Lotus Swipe Wrong Attempts  (Attempts: how many misses before correct)\n"
-            for i in 0..<lotusArrayToday.count{
-                a += String(i) + ": " + String(lotusArrayToday[i]) + ", "
-            }
-            self.swipesLabel.text = a
+//            self.timePlayedLabel.text = "Week: " + String(lotusTimeWeek) + "\nToday: " + String(lotusTimeToday)
+//            self.roundsPlayedLabel.text = "Week:\n" + String(lotusRoundsWeek) + "\nToday:\n" + String(lotusRoundsToday)
+//            var a = "Lotus Swipe Wrong Attempts  (Attempts: how many misses before correct)\n"
+//            for i in 0..<lotusArrayToday.count{
+//                a += String(i) + ": " + String(lotusArrayToday[i]) + ", "
+//            }
+//            self.swipesLabel.text = a
             
             let lotusCircleGraph = CircleChart(radius: circleGraphRadius, progressEndAngle: lotusGraphEndAngle, center: self.lotusGraphCenter, lineWidth: circleGraphWidth, outlineColor: lotusGraphOutColor, progressColor: lotusGraphProgColor)
             self.mainView.layer.addSublayer(lotusCircleGraph.outlineLayer)
