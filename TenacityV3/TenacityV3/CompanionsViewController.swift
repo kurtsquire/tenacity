352f68eb8a8efa885372f5f5605085b20ae3d8d1
Richie <richie@dhcp-v057-168.mobile.uci.edu>
2020-01-21 17:14:03 -0800

    partially done with unlocking pets
    2-12 done

--- TenacityV3/TenacityV3/CompanionsViewController.swift
+++ TenacityV3/TenacityV3/CompanionsViewController.swift
@@ -9,6 +9,7 @@
 
 import UIKit
 import WatchConnectivity
+import RealmSwift
 
 let petArray = ["bay", "fjol", "cleo", "eldur", "halo", "sage", "raz", "koko", "rio", "aqua", "royal", "indigo", "mar", "phoenix", "bly", "dioon", "toor", "reese", "navy", "iris", "loch", "river", "bahn", "zbut", "sierra", "echo", "nova", "jade", "koda", "bayou", "kolbi", "liyah", "rye", "qut", "yoko", "trii", "tala", "kitchi", "opal", "paytah", "gou", "axel", "wol", "sigma", "pi"]
 var petOwned = [1]
@@ -20,6 +21,8 @@ class CompanionsViewController: PhoneViewController {
     @IBOutlet var equipLabels: [UILabel]!
     @IBOutlet var companionsButtons: [UIButton]!
     var companionTutorialCompleted = false
+    
+    var achievementsCompleted: [Int] = []
     
     @IBAction func companionsButtonPressed(_ sender: UIButton) {
 
@@ -60,7 +63,8 @@ class CompanionsViewController: PhoneViewController {
     
     override func viewDidAppear(_ animated: Bool) { //opening back up the tab (works from other tabs)
         super.viewDidAppear(animated)
-        
+        testUserDefaults()
+        checkForAchievements()
         updatePetData()
         
         if !companionTutorialCompleted{
@@ -72,7 +76,6 @@ class CompanionsViewController: PhoneViewController {
     }
     
     func updatePetData(){
-        testUserDefaults()
         
         for equip in equipLabels {
             if (equip.tag == petEquipped + 1) {
@@ -113,6 +116,7 @@ class CompanionsViewController: PhoneViewController {
         petOwned = defaults.array(forKey: "petOwned") as? [Int] ?? [1]
         petEquipped = defaults.integer(forKey: "petEquipped")
         
+        
         companionTutorialCompleted = defaults.bool(forKey: "companionTutorialCompleted")
     }
     
@@ -130,12 +134,116 @@ class CompanionsViewController: PhoneViewController {
         }
     }
     
-    func checkForAchievement(){
+    func checkForAchievements(){
         //will check an achievements list for all completed achievements and make sure appropriate pets are unlocked
+        let AList = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+        for i in AList{
+            if !petOwned.contains(i){
+                checkConditions(x: i)
+            }
+        }
+        
     }
     
-    func checkConditions(){
+    func checkConditions(x: Int){
+        //check check specific achievement
+        let realm = try! Realm()
+        let bfx = realm.objects(GameDataModel.self).filter("gameTitle = %@", "breathe focus")
+        let bix = realm.objects(GameDataModel.self).filter("gameTitle = %@", "breathe infinite")
+        let lsx = realm.objects(GameDataModel.self).filter("gameTitle = %@", "lotus")
+        if x == 2{
+            if bfx.count > 0{
+                achievementComplete(x: 2)
+            }
+        }
+        else if x == 3{
+            if bfx.count > 0 && bix.count > 0 && lsx.count > 0{
+                achievementComplete(x: 3)
+            }
+        }
+        else if x >= 4 && x <= 6{
+            var time = 0.0
+            for item in bfx{
+                time += item.breatheFTimePlayed
+            }
+            if x == 4{
+                if time >= 20*60{
+                    achievementComplete(x: 4)
+                }
+            }
+            else if x == 5{
+                if time >= 60*60{
+                    achievementComplete(x: 5)
+                }
+            }
+            else if x == 6{
+                if time >= 180*60{
+                    achievementComplete(x: 6)
+                }
+            }
+        }
+        else if x >= 7 && x <= 9{
+            var breaths = 0
+            for item in bfx{
+                if item.gameDataType == "stop hold"{
+                    breaths += 1
+                }
+            }
+            if x == 7{
+                if breaths >= 10{
+                    achievementComplete(x: 7)
+                }
+            }
+            if x == 8{
+                if breaths >= 500{
+                    achievementComplete(x: 8)
+                }
+            }
+            if x == 9{
+                if breaths >= 2000{
+                    achievementComplete(x: 9)
+                }
+            }
+        }
+        else if x >= 10 && x <= 12{
+            var correct = 0
+            for item in bfx{
+                correct += item.breatheFCorrectSets
+            }
+            if x == 10{
+                if correct >= 2{
+                    achievementComplete(x: 10)
+                }
+            }
+            if x == 11{
+                if correct >= 100{
+                    achievementComplete(x: 11)
+                }
+            }
+            if x == 12{
+                if correct >= 500{
+                    achievementComplete(x: 12)
+                }
+            }
+        }
+        else if x == 13{
+            
+        }
+        else if x == 14{
+            
+        }
+        else if x == 15{
+            
+        }
+        //
+        
     }
+    
+    func achievementComplete(x: Int){
+        petOwned.append(x)
+        UserDefaults.standard.set(petOwned, forKey: "petOwned")
+    }
+
 }
 
 class CompanionsPopupController: PhoneViewController {
@@ -166,33 +274,33 @@ class CompanionsPopupController: PhoneViewController {
         }
         //
         else if (x == 6){
-            achievementLabel.text = "set a nudge"
+            achievementLabel.text = "Breathe 100 times in Breathe Focus"
         }
         else if (x == 7){
-            achievementLabel.text = "Use Breathe Focus within 5 minutes of a set nudge"
+            achievementLabel.text = "Breathe 500 times in Breathe Focus"
         }
         else if (x == 8){
-            achievementLabel.text = "Use Breathe Focus within 5 minutes of a set nudge 5 times total"
+            achievementLabel.text = "Breathe 2000 times in Breathe Focus"
         }
         //
         else if (x == 9){
-            achievementLabel.text = "Breathe 20 times in Breathe Focus"
+            achievementLabel.text = "Do 25 correct sets in Breathe Focus"
         }
         else if (x == 10){
-            achievementLabel.text = "Breathe 100 times in Breathe Focus"
+            achievementLabel.text = "Do 100 sets in Breathe Focus"
         }
         else if (x == 11){
-            achievementLabel.text = "Breathe 500 times in Breathe Focus"
+            achievementLabel.text = "Do 500 sets in Breathe Focus"
         }
         //
         else if (x == 12){
-            achievementLabel.text = "Do 10 sets in Breathe Focus"
+            achievementLabel.text = "set a nudge"
         }
         else if (x == 13){
-            achievementLabel.text = "Do 50 sets in Breathe Focus"
+            achievementLabel.text = "Use Breathe Focus within 5 minutes of a set nudge"
         }
         else if (x == 14){
-            achievementLabel.text = "Do 250 sets in Breathe Focus"
+            achievementLabel.text = "Use Breathe Focus within 5 minutes of a set nudge 5 times total"
         }
         //
         else if (x == 15){
